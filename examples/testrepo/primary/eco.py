461a66dca01ec372ea6f13b113d4bad3fcf2a997
Billy Newport <billy@billynewport.com>
2024-02-23 18:47:34 -0500

    More fluke stuff

--- examples/testrepo/primary/eco.py
+++
@@ -1,6 +0,0 @@
-from datasurface.md import Ecosystem
-from datasurface.md.GitOps import GitHubRepository
-
-def ecoFactory() -> Ecosystem:
-    e : Ecosystem = Ecosystem("Test", GitHubRepository("a", "b"))
-    return e
