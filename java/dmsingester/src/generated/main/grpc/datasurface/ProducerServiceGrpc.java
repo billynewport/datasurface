e0ae53d73d3981bcf844e760f190af8641f4584b
Billy Newport <billy@billynewport.com>
2025-01-22 16:37:24 -0500

    Reverting grpc and protobuf versions and generated code

--- java/dmsingester/src/generated/main/grpc/datasurface/ProducerServiceGrpc.java
+++ java/dmsingester/src/generated/main/grpc/datasurface/ProducerServiceGrpc.java
@@ -5,7 +5,7 @@ import static io.grpc.MethodDescriptor.generateFullMethodName;
 /**
  */
 @javax.annotation.Generated(
-    value = "by gRPC proto compiler (version 1.69.0)",
+    value = "by gRPC proto compiler (version 1.62.2)",
     comments = "Source: api.proto")
 @io.grpc.stub.annotations.GrpcGenerated
 public final class ProducerServiceGrpc {
