d9ddaef1070e62baa1bcb83d024be5848ba7bba6
Billy Newport <billy@billynewport.com>
2025-06-21 14:15:45 -0400

    gitignore for generated files during tests

--- src/tests/test_KubPGStarter.py
+++ src/tests/test_KubPGStarter.py
@@ -9,4 +9,4 @@ from datasurface.cmd.platform import generatePlatformBootstrap
 
 class Test_KubPGStarter(unittest.TestCase):
     def test_bootstrap(self):
-        generatePlatformBootstrap("src/tests/kubpgtests", "tests/kubpgtests/base", "Test_DP")
+        generatePlatformBootstrap("src/tests/kubpgtests", "src/tests/kubpgtests/base", "Test_DP")
